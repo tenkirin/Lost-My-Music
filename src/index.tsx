b595f8a70a3955ab47ffe1f0620e3f1d930f8e7a
tenkirin <60653216+tenkirin@users.noreply.github.com>
2023-02-22 18:29:12 +0900

    refactor: improve details

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
@@ -6,7 +6,7 @@ import App from './App';
 ReactDOM
   .createRoot(document.getElementById('root') as HTMLElement)
   .render(
-    <React.StrictMode>
+    <StrictMode>
       <App />
-    </React.StrictMode>
+    </StrictMode>
   );
